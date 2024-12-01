7e0a14936936695284ca8a4ef5e2bf9e77611d30
duccdev <duccybaka@gmail.com>
2024-12-01 08:02:11 +0300

    refactor: use enum

--- src/types/token.ts
+++ src/types/token.ts
@@ -1,17 +1,18 @@
-export type TokenType =
-  | "Keyword"
-  | "Identifier"
-  | "Operator"
-  | "OperatorCompound"
-  | "String"
-  | "Integer"
-  | "Float"
-  | "True"
-  | "False"
-  | "Null"
-  | "Punctuation"
-  | "Delimiter"
-  | "Whitespace";
+export enum TokenType {
+  Keyword = 0,
+  Identifier = 1,
+  Operator = 2,
+  OperatorCompound = 3,
+  String = 4,
+  Integer = 5,
+  Float = 6,
+  True = 7,
+  False = 8,
+  Null = 9,
+  Punctuation = 10,
+  Delimiter = 11,
+  Whitespace = 12,
+}
 
 export interface Token {
   type: TokenType;
